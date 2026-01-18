350c2adf3d25bd1aefa0132becf88c2fecc964b8
Raghad717 <r4gh4dalmalki@gmail.com>
2026-01-18 11:02:11 +0300

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Return a copy of the list with the element at idx replaced by element."""
+    """Return a copy of the list with the element at idx replaced.
+
+    Does not modify the original list.
+    """
     new_list = my_list.copy()
     if 0 <= idx < len(new_list):
         new_list[idx] = element
