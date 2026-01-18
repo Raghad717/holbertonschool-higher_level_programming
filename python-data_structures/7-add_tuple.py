f066f51250bbdcdc6b86586a3064915417e8d3d6
Raghad717 <r4gh4dalmalki@gmail.com>
2026-01-18 11:25:05 +0300

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    """Add two tuples, considering only the first two elements of each and filling missing elements with 0."""
+    """Add two tuples, considering only the first two elements of each
+    and filling missing elements with 0.
+    """
     a1 = tuple_a[0] if len(tuple_a) > 0 else 0
     a2 = tuple_a[1] if len(tuple_a) > 1 else 0
     b1 = tuple_b[0] if len(tuple_b) > 0 else 0
