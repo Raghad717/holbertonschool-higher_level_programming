e47664e9556d26b46e4408d7ab174a500d170ebd
Raghad717 <r4gh4dalmalki@gmail.com>
2026-01-19 11:02:00 +0300

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """Return a new list where all occurrences of search are replaced by replace."""
-    return [replace if x == search else x for x in my_list]
+    return [
+        replace if x == search else x
+        for x in my_list
+    ]
