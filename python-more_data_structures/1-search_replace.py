a19d5d56cb109a299e47c1f3a8560487a73cecfb
Raghad717 <r4gh4dalmalki@gmail.com>
2026-01-19 08:00:25 +0000

    Add function to replace all occurrences of an element in a list with a new value, returning a new list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """Return a new list where all occurrences of search are replaced by replace."""
+    return [replace if x == search else x for x in my_list]
