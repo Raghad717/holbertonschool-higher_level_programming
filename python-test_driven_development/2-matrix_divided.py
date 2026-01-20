1425412c4a03df53c5f7acaad6515d4d48edd209
Raghad717 <r4gh4dalmalki@gmail.com>
2026-01-20 12:33:37 +0300

    Update 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,24 +21,24 @@ def matrix_divided(matrix, div):
 
     # Check matrix type
     if not isinstance(matrix, list) or matrix == []:
-        raise TypeError("matrix must be a matrix (array of arrays of integers/floats)")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = None
     new_matrix = []
 
     for row in matrix:
         if not isinstance(row, list) or row == []:
-            raise TypeError("matrix must be a matrix (array of arrays of integers/floats)")
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
         if row_length is None:
             row_length = len(row)
         elif len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")  # FIXED capitalization
+            raise TypeError("Each row of the matrix must have the same size")
 
         new_row = []
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (array of arrays of integers/floats)")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             new_row.append(round(element / div, 2))
         new_matrix.append(new_row)
 
